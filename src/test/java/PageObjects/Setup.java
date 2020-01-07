1584ca505003447016c15b5ffd4de252eab8c381
Lucas Veloso <lucasveloso@MacBook-Pro-de-Lucas.local>
2020-01-07 10:12:53 -0300

    Include setup MacOS

--- src/test/java/PageObjects/Setup.java
+++ src/test/java/PageObjects/Setup.java
@@ -2,10 +2,14 @@ package PageObjects;
 
 public class Setup {
 	public static void DefaultSetup() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Selenium/chromedriver.exe");
+		//Start Windows
+		//System.setProperty("webdriver.chrome.driver", "C:\\Selenium/chromedriver.exe");
+		
+		//Start on Mac
+		System.setProperty("webdriver.chrome.driver", "../../eclipseLib/chromedriver");
 	
+		System.setProperty("webdriver.chrome.args", "--disable-logging");
 		System.setProperty("webdriver.chrome.args", "--disable-logging");
         System.setProperty("webdriver.chrome.silentOutput", "true");
 	}
 }
-

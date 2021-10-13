a9a96c497a982f990d2e09b014cb31fec9d97796
lucaslkveloso <lukaslk1>
2021-10-13 11:01:24 -0300

    Adjust to java 17.

--- src/test/java/PageObjects/HomePage.java
+++ src/test/java/PageObjects/HomePage.java
@@ -10,16 +10,13 @@ public class HomePage {
 	public HomePage(WebDriver driver){
 		this.driver = driver;
 	}
-	
-	//Get the User name from Home Page
+
 		public void getFirstProduct(){
 	        
 			driver.findElement(firstProduct).click();
 		}
 		public void ClickFirstProduct(){
-			//Click Login button
-			this.getFirstProduct();		
-			
+			this.getFirstProduct();
 		}
 }
 
--- src/test/java/PageObjects/Setup.java
+++ src/test/java/PageObjects/Setup.java
@@ -1,15 +1,28 @@
 package PageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class Setup {
+	public static WebDriver driver;
+	public static HomePage objHomePage;
+	public static Register objRegister;
+
 	public static void DefaultSetup() {
 		//Start Windows
 		//System.setProperty("webdriver.chrome.driver", "C:\\Selenium/chromedriver.exe");
 		
 		//Start on Mac
-		System.setProperty("webdriver.chrome.driver", "E:/Java/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "E:Java\\chromedriver.exe");
 	
 		System.setProperty("webdriver.chrome.args", "--disable-logging");
 		System.setProperty("webdriver.chrome.args", "--disable-logging");
         System.setProperty("webdriver.chrome.silentOutput", "true");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://demo.automationtesting.in/");
+		driver.manage().window().maximize();
 	}
 }
